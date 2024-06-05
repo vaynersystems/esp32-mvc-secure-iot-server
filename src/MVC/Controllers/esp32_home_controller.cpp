716a262f9a9fcdb9858ddaf14b9bda2e10073d66
Lev Vayner <lev.vayner@vaynersystems.com>
2024-06-05 17:09:53 -0400

    cleaning up. nicer home page

--- src/MVC/Controllers/esp32_home_controller.cpp
+++ src/MVC/Controllers/esp32_home_controller.cpp
@@ -7,30 +7,30 @@ void esp32_home_controller::Index(HTTPRequest* req, HTTPResponse* res) {
     
     title = "ESP32 Web Server Home Page";
 
-    string ctrString = "[";
-    int numOfControllers = BaseControllerFactory::getInstanceCount();
-    //Serial.printf("Found %i controllers\n", numOfControllers);
-    for(int i=0;i< numOfControllers;i++){
-        auto controller = BaseControllerFactory::getInstanceAt(i);
-        if(controller.first[0] == '_') continue;
+    // string ctrString = "[";
+    // int numOfControllers = BaseControllerFactory::getInstanceCount();
+    // //Serial.printf("Found %i controllers\n", numOfControllers);
+    // for(int i=0;i< numOfControllers;i++){
+    //     auto controller = BaseControllerFactory::getInstanceAt(i);
+    //     if(controller.first[0] == '_') continue;
 
-        vector<string> actions = {};
-        auto genController = controller.second();
-        genController->GetActions(&actions);
-        for(const string& action : actions)     
-             ctrString += "{\"" + controller.first + "\": \"" + action + "\"}, ";
+    //     vector<string> actions = {};
+    //     auto genController = controller.second();
+    //     genController->GetActions(&actions);
+    //     for(const string& action : actions)     
+    //          ctrString += "{\"" + controller.first + "\": \"" + action + "\"}, ";
 
-        actions.clear();
+    //     actions.clear();
        
-        delete genController; 
-    }
+    //     delete genController; 
+    // }
     
-    if(ctrString.length() > 2) //trim off trailing comma
-        ctrString = ctrString.substr(0,ctrString.length() - 2); 
+    // if(ctrString.length() > 2) //trim off trailing comma
+    //     ctrString = ctrString.substr(0,ctrString.length() - 2); 
 
-        ctrString += "]";
+    //     ctrString += "]";
     
-    controllerTemplate.SetTemplateVariable("$_Controllers", ctrString.c_str());
+    // controllerTemplate.SetTemplateVariable("$_Controllers", ctrString.c_str());
     
     esp32_base_controller::Index(req,res);    
 }
