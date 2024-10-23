741718d5b39b8a6ea3bcaa6a875c93b889fbef1e
Lev Vayner <lev.vayner@vaynersystems.com>
2024-10-22 21:18:19 -0400

    minor fixes

--- src/System/CORE/esp32_server.cpp
+++ src/System/CORE/esp32_server.cpp
@@ -95,36 +95,7 @@ bool esp32_server::start() {
         secureServer->start();
 
     unsecureServer->start();
-
-    // //OTA
-    // ArduinoOTA
-    // .onStart([]() {
-    //     String type;
-    //     if (ArduinoOTA.getCommand() == U_FLASH)
-    //         type = "sketch";
-    //     else // U_SPIFFS
-    //         type = "filesystem";
-
-    //     // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-    //     Serial.println("Start updating " + type);
-    // })
-    // .onEnd([]() {
-    //   Serial.println("\nEnd");
-    // })
-    // .onProgress([](unsigned int progress, unsigned int total) {
-    //   Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    // })
-    // .onError([](ota_error_t error) {
-    //   Serial.printf("Error[%u]: ", error);
-    //   if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    //   else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    //   else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    //   else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    //   else if (error == OTA_END_ERROR) Serial.println("End Failed");
-    // });
-
-    // ArduinoOTA.begin();
-
+  
     bool isReady =  (!_enableSSL || secureServer->isRunning()) && unsecureServer->isRunning();
     if (isReady) {
         #ifdef DEBUG
@@ -146,7 +117,7 @@ bool esp32_server::stop()
 bool esp32_server::isRunning() {
     return (!_enableSSL || secureServer->isRunning()) && unsecureServer->isRunning();
 }
-
+unsigned long _lastReporStatus = 0;
 void esp32_server::step()
 {
     if(_enableSSL)
@@ -157,6 +128,14 @@ void esp32_server::step()
         logger.logInfo("Restarting controller due to configuration 'restartAfter' parameter.");
         esp_restart();
     } 
+
+    if(_lastReporStatus + 10000 < millis()){
+        _lastReporStatus = millis();
+        Serial.printf("Server Staus: %s.\nSecure Server Status %s\n",
+            unsecureServer->isRunning() ? "Running" : "Stopped",
+            secureServer->isRunning() ? "Running" : "Stopped"
+        );
+    }
         
 }
 
